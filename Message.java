727abc3700c7ebfedaec67e6b1e4eddaea1cafd0
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 18:37:14 -0500

    initial commit

---
+++ Message.java
@@ -0,0 +1,7 @@
+public class CIS2323_Week02_Quiz {
+	public static void main(String[] args)
+	{
+		System.out.println("Christian Reyes");
+		System.out.println("I learned the java compiler, javac");
+	}
+}
